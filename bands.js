8beb66b93938b24e5d0a105f59619e744c5df9b7
Mel999666 <melina999@hotmail.com>
2025-10-26 14:18:08 +0200

    Update bands.js

--- bands.js
+++ bands.js
@@ -1,67 +1,149 @@
 // bands.js
 // Fill or edit as needed. fbPageId and igUserId only if you manage those accounts.
 window.BANDS = [
-  { name: "Black Strays", genre: "Hard Rock", city: "Limassol",
+  {
+    name: "Black Strays",
+    genre: "Hard Rock",
+    city: "Limassol",
     youtube: "https://www.youtube.com/watch?v=P1BLK1hKqCM",
-    facebookUrl: "", spotify: "https://open.spotify.com/artist/3wd5EEv0qSkYnzSOMBZDPA",
+    facebookUrl: "",
+    spotify: "https://open.spotify.com/artist/3wd5EEv0qSkYnzSOMBZDPA",
     instagramUrl: "https://www.instagram.com/blackstraysband/",
-    fbPageId: "", igUserId: "" },
+    fbPageId: "",
+    igUserId: "",
+    manualStats: {
+      // Facebook not available
+      instagramFollowers: 8522
+    }
+  },
 
-  { name: "Guiltera", genre: "Metalcore 4. Wave", city: "Limassol",
+  {
+    name: "Guiltera",
+    genre: "Metalcore 4. Wave",
+    city: "Limassol",
     youtube: "https://www.youtube.com/watch?v=gvQGIsr6oHc",
     facebookUrl: "https://www.facebook.com/profile.php?id=61559603642016",
     spotify: "https://open.spotify.com/artist/3Zn5DaI82ti9N0TLUd3LqQ",
     instagramUrl: "https://www.instagram.com/guiltera_band/",
-    fbPageId: "", igUserId: "" },
+    fbPageId: "",
+    igUserId: "",
+    manualStats: {
+      facebookFollowers: 78,
+      instagramFollowers: 3546
+    }
+  },
 
-  { name: "Ka'aper", genre: "Dark Metal", city: "Limassol",
+  {
+    name: "Ka'aper",
+    genre: "Dark Metal",
+    city: "Limassol",
     youtube: "https://www.youtube.com/watch?v=ASzy9bWighQ",
     facebookUrl: "https://www.facebook.com/profile.php?id=61561403806389",
     spotify: "https://open.spotify.com/artist/4T0bImsYNQXp9JdY1RBzEu",
     instagramUrl: "https://www.instagram.com/kaaper_official/",
-    fbPageId: "", igUserId: "" },
+    fbPageId: "",
+    igUserId: "",
+    manualStats: {
+      facebookFollowers: 236,
+      instagramFollowers: 1616
+    }
+  },
 
-  { name: "Leave the Wave", genre: "Hard Rock", city: "Nicosia",
+  {
+    name: "Leave the Wave",
+    genre: "Hard Rock",
+    city: "Nicosia",
     youtube: "https://www.youtube.com/watch?v=Ocef41DXZOk",
     facebookUrl: "https://www.facebook.com/share/1HDsTp1voH/?mibextid=wwXIfr",
     // Their link points to YouTube Music. We will still handle it.
     spotify: "https://youtube.com/@leavethewave?si=V7VJqZ_srN9cxGxw",
     instagramUrl: "https://www.instagram.com/leavethewave/",
-    fbPageId: "", igUserId: "" },
+    fbPageId: "",
+    igUserId: "",
+    manualStats: {
+      facebookFollowers: 398,
+      instagramFollowers: 1126
+    }
+  },
 
-  { name: "LIOMENO TOU", genre: "Stoner Metal", city: "Nicosia",
+  {
+    name: "LIOMENO TOU",
+    genre: "Stoner Metal",
+    city: "Nicosia",
     youtube: "",
     facebookUrl: "",
     spotify: "https://open.spotify.com/track/0oQXAm5hYJnQE9J5UHWcUO",
     instagramUrl: "https://www.instagram.com/liomeno.tou",
-    fbPageId: "", igUserId: "" },
+    fbPageId: "",
+    igUserId: "",
+    manualStats: {
+      // Facebook not available
+      instagramFollowers: 70
+    }
+  },
 
-  { name: "MARANG.", genre: "Progressive Metal", city: "Nicosia",
+  {
+    name: "MARANG.",
+    genre: "Progressive Metal",
+    city: "Nicosia",
     youtube: "https://www.youtube.com/watch?v=auaVFa2oX7A",
     facebookUrl: "https://www.facebook.com/share/17VciyNjMH/?mibextid=wwXIfr",
     spotify: "https://spotify.link/7ranN7gVFXb",
     instagramUrl: "https://www.instagram.com/stephanosmarangos/",
-    fbPageId: "", igUserId: "" },
+    fbPageId: "",
+    igUserId: "",
+    manualStats: {
+      // Facebook not available
+      instagramFollowers: 2403
+    }
+  },
 
-  { name: "Salienzor", genre: "Metalcore 3. Wave", city: "Nicosia",
+  {
+    name: "Salienzor",
+    genre: "Metalcore 3. Wave",
+    city: "Nicosia",
     youtube: "https://www.youtube.com/watch?v=P-rovmbnWFY",
     facebookUrl: "https://www.facebook.com/profile.php?id=61552235609980",
     spotify: "https://open.spotify.com/artist/3z5HqSTma8Jo3riI30C8NI",
     instagramUrl: "https://www.instagram.com/salienzor/",
-    fbPageId: "", igUserId: "" },
+    fbPageId: "",
+    igUserId: "",
+    manualStats: {
+      facebookFollowers: 18,
+      instagramFollowers: 337
+    }
+  },
 
-  { name: "Speak In Whispers", genre: "Groove Metal", city: "Larnaka",
+  {
+    name: "Speak In Whispers",
+    genre: "Groove Metal",
+    city: "Larnaka",
     youtube: "https://www.youtube.com/watch?v=AQ7C-HvyDhY",
     // You gave an Instagram link in the facebook field. I keep it in instagramUrl.
     facebookUrl: "",
     spotify: "https://open.spotify.com/artist/61Usxx24TwxGUlTUhE2S4u",
     instagramUrl: "https://www.instagram.com/speak.in.whispers/",
-    fbPageId: "", igUserId: "" },
+    fbPageId: "",
+    igUserId: "",
+    manualStats: {
+      facebookFollowers: 1300,
+      instagramFollowers: 1091
+    }
+  },
 
-  { name: "YMNKY!", genre: "Alternative Metal", city: "Limassol",
+  {
+    name: "YMNKY!",
+    genre: "Alternative Metal",
+    city: "Limassol",
     youtube: "https://music.youtube.com/watch?v=rEZiJ5eJZIs",
     facebookUrl: "https://www.facebook.com/ymnky.bnd",
     spotify: "https://open.spotify.com/artist/06EcVA3ORp4U1w2k29yVzG",
     instagramUrl: "https://www.instagram.com/ymnky_band/",
-    fbPageId: "", igUserId: "" }
+    fbPageId: "",
+    igUserId: "",
+    manualStats: {
+      facebookFollowers: 48,
+      instagramFollowers: 2648
+    }
+  }
 ];
